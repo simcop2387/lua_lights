5191851d0bf8aebcb65505029faa889e3b8044c6
Ryan Voots <simcop2387@simcop2387.info>
2014-11-27 22:43:29 -0800

    Starting to get the wifi code going

--- Lualights.ino
+++ Lualights.ino
@@ -6,11 +6,12 @@
  */
 
 #include "lua.hpp"
+#include "wifi.h"
      
 void setup() {
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(13, OUTPUT);
+  //pinMode(13, OUTPUT);
   Serial.begin(9600);
   delay(10000);
   
@@ -20,8 +21,8 @@ void setup() {
 void loop() {
   l_frame();
   
-  digitalWrite(13, HIGH);   // set the LED on
-  delay(100);              // wait for a second
-  digitalWrite(13, LOW);    // set the LED off
-  delay(100);              // wait for a second
+  //digitalWrite(13, HIGH);   // set the LED on
+  //delay(100);              // wait for a second
+  //digitalWrite(13, LOW);    // set the LED off
+  //delay(100);              // wait for a second
 }
---
+++ log.h
@@ -0,0 +1,3 @@
+  // This line defines a "Uart" object to access the serial port
+//  HardwareSerial CommOut = HardwareSerial();
+  usb_serial_class LogOut = Serial;
